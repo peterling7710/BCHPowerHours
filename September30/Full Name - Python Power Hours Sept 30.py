35918325aa952c3d716906ca2e98522cd4c86ea6
Seamus Ingram <seamusingram@Seamuss-MacBook-Air.local>
2020-09-29 16:40:46 -0400

    Sept 30 :computer:

---
+++ September30/Full Name - Python Power Hours Sept 30.py
@@ -0,0 +1,23 @@
+# Creata a todo class - parameters name, date and complete
+# You can put all your methods for what the app should do (from the spec) here as well!
+
+# Have an infinite loop
+
+# Read user input
+
+# Decision structure based on input
+# Close app
+
+# Add a new todo - use input again
+
+# Mark todo as complete - need input 
+
+# Print out todos
+
+# Print completed todos
+
+# Print incompleted todos
+
+# EXTRA CHALLENGE Delete a todo - takes input as well
+
+# EXTRA CHALLENGE - Print overdue
