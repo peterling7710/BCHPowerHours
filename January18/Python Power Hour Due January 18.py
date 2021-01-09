bd852f4f0d7ccc8e8cb2a2c21861dee29495a194
Seamus Ingram <seamus.ingram@gmail.com>
2021-01-08 22:34:07 -0500

    Jan 18 Content

---
+++ January18/Python Power Hour Due January 18.py
@@ -0,0 +1,56 @@
+# Let's create a way to store a collection of data and functions to organise our stocks
+# We should store the stock ticker (it's name), it's current price, the price we bought it at,
+# and the price history. It might be good to have it's sector as well
+
+# Create a new stock
+
+# Change the price of the stock, 
+# let's do it 3 ways. Either, providing a new price,
+# The monetary amount it changed by, or the percentage change
+# Make sure to add any changes to the price history!
+
+# Calculate ROI
+
+# Just like in Python 1, creating a text output to describe our stock will be nice to read!
+
+# Display the price history of the stock, as tracked by your changes
+
+# Now, let's create methods that help us work with a list of stocks
+# total the value of all stocks
+
+# total how much you bought portfolio for
+
+# calculate your total return
+
+# Determine which sectors you're invested in
+
+# Use this to detemine the amount invested in each sector
+
+# OPTIONAL - Make an app to test your code! 
+# If you don't want to go through the trouble
+# Hard code in a couple of stocks, collect them together,
+# and test all the methods and functions you made!
+
+######################################################################################################
+# Provide a nice prompt
+
+# Read user input
+
+# Decision structure based on input
+# Close app
+
+# Add a new stock - use input again
+
+# Mark todo as complete - need input 
+
+# Check Portfolio value
+
+# Check your portfolio return
+
+# Print specific stock return
+
+# Print sectors invested in
+
+# Print investment by sector
+
+# Sell stock
