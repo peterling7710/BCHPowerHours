07215f34042da3f785712777357ef14f9c6e0546
peterling7710 <PLing3@its.jnj.com>
2021-01-12 00:10:51 -0500

    Reformatted for 2021, Added Jan 18th session

---
+++ 2021/January18/Python Challenge - Jan182021.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Python Challenge - Consumer Analytics 
+# 
+# - From: https://www.kaggle.com/blastchar/telco-customer-churn
+# - Each row represents a customer, each column contains customer’s attributes described on the column Metadata.
+# - The raw data contains 7043 rows (customers) and 21 columns (features).
+# - The “Churn” column is our target.
+
+# In[ ]:
+
+
+# Start by importing all the packages we might need
+# Import Pandas and Matplotlib
+
+
+# In[ ]:
+
+
+#Read the data into a variable
+
+
+# In[ ]:
+
+
+#Let's see how much data we're working with, print the dimensions of the dataset
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+# Investigate the gender distribution of our data(# Men vs. Women)
+
+
+# In[ ]:
+
+
+# Let's do the same for the Payment Methods. 
+# Calculate and plot the different Payment Methods.
+
+
+# In[ ]:
+
+
+#In the "Churn" column, replace "Yes" values with 1 and "No" values with 0
+
+
+# In[ ]:
+
+
+# Create a Pivot Table measuring the amount of customer churn by gender and Payment Method
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+#Visualize your pivot table, make sure to add a title, and to size your graph appropriately
+
+
+#Export your graph to JPG/PNG
+
+
+# In[ ]:
+
+
+#Q: In 1-2 senteces, explain your findings from your graph.
+#A: 
+
